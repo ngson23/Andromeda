d23d6cc38a86e44e532922bb9e05160655ea793f
HoangHe <thauminhphong191@gmail.com>
2022-11-30 01:38:02 +0700

    Profile và Actionbar

--- app/src/main/java/vn/edu/poly/andromeda/MainActivity.java
+++ app/src/main/java/vn/edu/poly/andromeda/MainActivity.java
@@ -1,20 +1,15 @@
 package vn.edu.poly.andromeda;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
 import vn.edu.poly.andromeda.fragment.DownloadFragment;
 import vn.edu.poly.andromeda.fragment.ForumFragment;
 import vn.edu.poly.andromeda.fragment.HomeFragment;
--- app/src/main/java/vn/edu/poly/andromeda/fragment/HomeFragment.java
+++ app/src/main/java/vn/edu/poly/andromeda/fragment/HomeFragment.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
