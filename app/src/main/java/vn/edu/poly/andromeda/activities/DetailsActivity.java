3743175a76ee1658d70c4568b55aa13535a51474
HoangHe <thauminhphong191@gmail.com>
2022-12-10 19:13:49 +0700

    Logo Actionbar Hoàn thành

--- app/src/main/java/vn/edu/poly/andromeda/activities/DetailsActivity.java
+++ app/src/main/java/vn/edu/poly/andromeda/activities/DetailsActivity.java
@@ -20,6 +20,8 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
